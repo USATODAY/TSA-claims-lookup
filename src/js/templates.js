3cf26c6ee86166f19baacf542765c5be0e03303e
mitchthorson <mthorson@usatoday.com>
2015-06-26 13:26:56 -0400

    add template

---
+++ src/js/templates.js
@@ -0,0 +1,29 @@
+define(function(){
+
+this["templates"] = this["templates"] || {};
+
+this["templates"]["app.html"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<div class="wrap mobile-wrap" id="iapp-app" ng-controller="SearchController as search" ng-click="isFormOpen = false">\n\n    <div class="panel-inner-wrap">\n      <div class="mobile-back-bar"></div>\n      <div class="inner-wrap">\n        <h1 class="mobile-head">Hazards on the runway</h1>\n        <h3 class="sub-head">Search for incidents at your airport</h3>\n      </div>\n      \n      <div class="inner-wrap">\n        <div id="search_cont" class="pull-left" >\n          <input type="text" class="form-control" id="company_search_box" placeholder="Enter your airport here" ng-model="filterTerm"  ng-change="search.mobileSearch()" >\n          <!-- <div class="clear-button" ng-click ="search.clear()"><img src="img/close-icon.svg" alt=""></div> -->\n          <ul class="list-group" ng-show="isFormOpen" id="search_results" ng-focus="isFormOpen = True">\n              <li class="list-group-item ng-cloak" ng-repeat="airport in filteredArray" ng-click="setFocus(airport)">\n                  <h3 class="search-name">{{airport.name}}</h3>\n                  <h4 class="search-detail">{{airport.st}}</h4>\n                  \n              </li>\n          </ul>\n          <div class="mobile-company-info-box">Use the search box above to find your airport.</div>\n            </div>\n      </div>\n      \n      <div class="panel-wrap" ng-show="isDetailShow">\n        <div class="panel-info inner-wrap">\n            <h3 class="data-head" id="panel-head">{{current.name}}</h3>\n            <h4 class="data-sub" id="panel-sub">{{current.st}}</h4>\n          </div>\n          <div class="data-chart"></div>\n          <div class="data-group inner-wrap group">\n            <h2 class="total-number">{{current.tot}}</h2>\n            <h2 class="total-label">Total Incidents</h2>\n            \n            <div class="sub-group">\n              <h4 class="data-number">{{current.com}}</h4>\n              <h3 class="section-head">Incidents involving US commercial airlines</h3>\n              \n            </div>\n            <div class="sub-group">\n              <h4 class="data-number">{{current.sev}}</h4>\n              <h3 class="section-head incident-label" ng-click="showInfo()">severe incidents</h3>\n              \n            </div>\n             \n          </div>\n        <div class="inner-wrap buttom-buttons">\n          <div class="info-button" ng-click="showInfo()"><img src="http://www.gannett-cdn.com/experiments/usatoday/2014/11/airport-interactive/img/info.svg" alt="info" ></div>\n          <a href="http://www.usatoday.com/story/news/nation/2014/11/24/runway-close-calls-increasing/19477871/" target="_blank" class="article-button button" >Read Story</a>\n          <div class="share-button" ng-click="showShare()"><img src="http://www.gannett-cdn.com/experiments/usatoday/2014/11/airport-interactive/img/options.svg" alt="share" ></div>\n        </div>\n      </div>      \n    </div>\n    <div class="share-page">\n        <div class="share-close-button" ng-click="hideShare()"><img src="http://www.gannett-cdn.com/experiments/usatoday/2014/11/airport-interactive/img/close.svg" alt="close" class="close-icon"></div>\n        <h2 class="share-title">Share</h2>\n        <p class="share-copy">{{share.copy}}</p>\n            \n        <a href="{{share.tweetUrl}}" onclick="Analytics.click(\'Twitter Share\')" class=\'social-link\' id=\'twitter-share\'> <img src=\'http://www.gannett-cdn.com/experiments/usatoday/2014/11/airport-interactive/img/twitter.svg\' alt="twitter" class="social-icon"></a>\n        <a href="{{share.fbUrl}}" target="popup"><img src=\'http://www.gannett-cdn.com/experiments/usatoday/2014/11/airport-interactive/img/fb.svg\' alt="twitter" class="social-icon"></a>\n        <a href="{{share.emailURL}}" onclick="Analytics.click(\'Email Share\')" class="social-link" id="email-share"><img src="http://www.gannett-cdn.com/experiments/usatoday/2014/11/airport-interactive/img/email.svg" alt="email" class="social-icon">\n          </a>\n      \n      </div>\n    <div class="info-page">\n      <div class="info-close-button" ng-click="hideInfo()"><img src="http://www.gannett-cdn.com/experiments/usatoday/2014/11/airport-interactive/img/close.svg" alt="close" class="close-icon"></div>\n      <h2 class="info-title share-title">Info</h2>\n      <p class="info-text">Nearly once a day, a commercial airplane encounters a collision hazard at a U.S. airport, when another aircraft, a vehicle or a person is on an active runway. You can look up the number of such incidents catalogued by the Federal Aviation Administration since 2001, as well as the number of those incidents that involved a U.S. commercial airline and the number that the FAA deemed were in the two most severe categories.</p>\n      <p class="info-text">"Severe" incidents include "incidents in which a collision was narrowly avoided", and incidents "in which separation decreases and there is a significant potential for collision, which may result in a time critical corrective/evasive response to avoid a collision.</p>\n      <h2 class="info-title share-title">Credits</h2>\n      <p class="info-text">Steve Reilly, Tory Hargro, Mitchell Thorson USA TODAY</p>\n      <p class="info-text"><strong>Source: </strong> FAA</p>\n      <p class="info-text"><strong>Photo Credits: </strong>KARIM SAHIB/AFP/Getty Images</p>\n    </div>\n\n  </div>\n';
+
+}
+return __p
+};
+
+this["templates"]["template.html"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<h3>' +
+((__t = (test)) == null ? '' : __t) +
+'</h3>\n';
+
+}
+return __p
+};
+
+  return this["templates"];
+
+});
